2a03aef9b14035fc90a60c974dd4a7886f33d28c
Jeffery Cavallaro <jeffery@cavcom.com>
2021-02-18 07:03:02 -0800

    Extending input library

--- C++/Primer/ch01/add.cc
+++ C++/Primer/ch01/add.cc
@@ -4,10 +4,19 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <stdexcept>
 
 int main(int argc, char *argv[]) {
-    int value1 = input_integer("Enter first addend: ");
-    int value2 = input_integer("Enter second addend: ");
+    int value1 = 0;
+    int value2 = 0;
+    try {
+        value1 = input_integer("Enter first addend: ");
+        value2 = input_integer("Enter second addend: ");
+    }
+    catch (const std::runtime_error &sre) {
+        std::cout << sre.what() << std::endl;
+        return EXIT_FAILURE;
+    }
     int sum = value1 + value2;
     std::cout << "Sum: " << sum << std::endl;
     return EXIT_SUCCESS;
--- C++/Primer/ch01/input.cc
+++ C++/Primer/ch01/input.cc
@@ -1,11 +1,20 @@
 #include "input.h"
 
-#include <iostream>
+#include <sstream>
+#include <stdexcept>
+
+int string_integer(const std::string &in) {
+    std::istringstream sin(in);
+    return input_integer(sin);
+}
 
 int input_integer(const std::string &prompt) {
     std::cout << prompt;
     std::cout.flush();
     int value = 0;
     std::cin >> value;
+    if (!std::cin.good()) {
+        throw std::runtime_error("Invalid input value");
+    }
     return value;
 }
