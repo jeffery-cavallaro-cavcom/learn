3947b140364876607cbcf08b201faf5a4df92809
Jeffery Cavallaro <jeffery@grumpy.cavcom.com>
2021-09-25 13:14:56 -0700

    Add gitignore

---
+++ C++/NovToProf/ch01/answer.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    int answer {42};
+    std::cout << "The answer to life, the universe, and everything is "
+              << answer
+              << std::endl;
+    return 0;
+}
