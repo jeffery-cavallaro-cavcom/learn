34a455527284888430a71089539ace7a1130c9c6
Jeffery Cavallaro <jeffery@cavcom.com>
2022-12-30 17:33:10 -0800

    Fix import issues

--- TclTk/Tkinter/data_entry/data_entry.py
+++ TclTk/Tkinter/data_entry/data_entry.py
@@ -2,6 +2,16 @@
 Data Entry Demonstration
 """
 
+from pathlib import Path
+import sys
+
+PARENT = Path(__file__).parent
+sys.path = [
+    str(PARENT / 'data_entry'),
+    str(PARENT / 'data_entry' / 'widgets'),
+    *sys.path
+]
+
 from data_entry.application import Application
 
 Application().mainloop()
